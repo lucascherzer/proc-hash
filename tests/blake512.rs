8a02c2cc7790bdb8a2be1854998f480c9d3e14f4
Luca Scherzer <83914554+lucascherzer@users.noreply.github.com>
2023-07-13 19:55:14 +0200

    add semicolon

--- tests/blake512.rs
+++ tests/blake512.rs
@@ -6,5 +6,5 @@ fn test_blake512() {
     assert_eq!(
         include_blake512!("https://www.youtube.com/watch?v=dQw4w9WgXcQ"),
         "7eda814eb5fe31a58a639842aebf554b505bb5e65bdcd1052ee035a1227d353c590580b49c453606e268b4a4f0c7862dff5fa748cd4b0e60c1bcd77c92dd7fd8"
-    )
+    );
 }
--- tests/md5.rs
+++ tests/md5.rs
@@ -6,5 +6,5 @@ fn test_md5() {
     assert_eq!(
         include_md5!("https://www.youtube.com/watch?v=dQw4w9WgXcQ"),
         "75170fc230cd88f32e475ff4087f81d9"
-    )
+    );
 }
